a2d2be078d2d4e73bf5da4c156d07f1c99bde0c2
Dom <dom.benatar@hotmail.com>
2023-10-30 14:13:43 +0000

    .env added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+import { useState } from 'react';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+  async function getLocation(event) {
+    event.preventDefault();
+  }
+
+  const API = ``
+
+  const res = await axios.get(API);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>API's</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Location" />
+        <button>Get Location</button>
+      </form>
+
+      <h2>{location.display_name}</h2>
+
+
     </>
   )
 }
